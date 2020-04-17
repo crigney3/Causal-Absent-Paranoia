62057f9bd19780dcfa14d4ceeaf43130789a1d85
Corey Rigney <coreyrigney@gmail.com>
2020-04-17 00:25:43 -0500

    Initial commit, testing lighting and node scripts

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : Node
+{
+    public List<Node> relevantNodes = new List<Node>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
